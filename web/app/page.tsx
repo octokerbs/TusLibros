fc18496cb2d5e3b1817cd1f9d3a5d7e0ca103906
KerbsOD <kerbsod@gmail.com>
2024-12-15 16:29:10 -0300

    App

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-        return <h1>Hello, Next.js!</h1>
+import Box from "@mui/material/Box";
+import BookGrid from "./components/Grid";
+import Header from "./components/HeaderBar";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TusLibros",
+  description: "BookShop built in Go and Nextjs with TDD",
+};
+
+export default function Home() {
+  return (
+    <Box sx={{ bgcolor: "#F3FCF0", width: "100vw", overflow: "auto" }}>
+      <Header></Header>
+      <BookGrid></BookGrid>
+    </Box>
+  );
 }
