8df1ed62eb93fb2c21484b29d0c13564095c2c8a
okerbs <kerbsod@gmail.com>
2025-01-15 04:13:36 -0300

    Popup alert with customized color for success and error severity

--- web/app/components/CheckoutPopup/useAlert.tsx
+++ web/app/components/CheckoutPopup/useAlert.tsx
@@ -2,42 +2,44 @@ import Alert from "@mui/material/Alert";
 import { useCallback, useState } from "react";
 
 export const useAlert = (onClose: () => void) => {
-        const [alertState, setAlertState] = useState(
+    const [alertState, setAlertState] = useState(
+        <Alert
+            onClose={onClose}
+            severity="warning"
+            variant="filled"
+            sx={{
+                width: "92%",
+                marginTop: "5.5vh",
+                bgcolor: "#567568",
+            }}
+        >
+            No transaction could be done!
+        </Alert>
+    );
+
+    const handleState = useCallback(
+        (severityCode: "error" | "success", message: string) => {
+            let color = "#567568";
+            if (severityCode == "error") {
+                color = "#C14431";
+            }
+            setAlertState(
                 <Alert
-                        onClose={onClose}
-                        severity="warning"
-                        variant="filled"
-                        sx={{
-                                width: "92%",
-                                marginTop: "5.5vh",
-                                bgcolor: "#567568",
-                        }}
+                    onClose={onClose}
+                    severity={severityCode}
+                    variant="filled"
+                    sx={{
+                        width: "17vw",
+                        marginTop: "5.5vh",
+                        bgcolor: color,
+                    }}
                 >
-                        No transaction could be done!
+                    {message}
                 </Alert>
-        );
+            );
+        },
+        [onClose]
+    );
 
-        const handleState = useCallback(
-                (
-                        severityCode: "error" | "warning" | "success",
-                        message: string
-                ) => {
-                        setAlertState(
-                                <Alert
-                                        onClose={onClose}
-                                        severity={severityCode}
-                                        variant="filled"
-                                        sx={{
-                                                width: "17vw",
-                                                marginTop: "5.5vh",
-                                        }}
-                                >
-                                        {message}
-                                </Alert>
-                        );
-                },
-                [onClose]
-        );
-
-        return { alertState, handleState };
+    return { alertState, handleState };
 };
