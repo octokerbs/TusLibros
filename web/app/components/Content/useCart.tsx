06321fb21c3ce127133c79fcb19b91269be7917e
okerbs <kerbsod@gmail.com>
2025-01-15 04:14:04 -0300

    Errors are now propagated to the component level

--- web/app/components/Content/useCart.tsx
+++ web/app/components/Content/useCart.tsx
@@ -2,27 +2,27 @@ import { useCallback, useState } from "react";
 import { api } from "../utils/api";
 
 export default function useCart() {
-        const [cart, setCart] = useState<Record<string, number>>({});
-        const [cartID, setCartID] = useState<number>(-1);
+    const [cart, setCart] = useState<Record<string, number>>({});
+    const [cartID, setCartID] = useState<number>(-1);
 
-        const requestCartID = useCallback(async () => {
-                try {
-                        const cartID = await api.createCart("Octo", "Kerbs");
-                        setCartID(cartID);
-                        setCart({});
-                } catch (error) {
-                        console.error("Cart initialization failed:", error);
-                }
-        }, []);
+    const requestCartID = useCallback(async () => {
+        try {
+            const cartID = await api.createCart("Octo", "Kerbs");
+            setCartID(cartID);
+            setCart({});
+        } catch (error) {
+            throw error;
+        }
+    }, []);
 
-        const requestCartItems = useCallback(async () => {
-                try {
-                        const items = await api.listCart(cartID);
-                        setCart(items);
-                } catch (error) {
-                        console.error("Cart fetch error: ", error);
-                }
-        }, [cartID]);
+    const requestCartItems = useCallback(async () => {
+        try {
+            const items = await api.listCart(cartID);
+            setCart(items);
+        } catch (error) {
+            throw error;
+        }
+    }, [cartID]);
 
-        return { cart, cartID, requestCartID, requestCartItems };
+    return { cart, cartID, requestCartID, requestCartItems };
 }
--- web/app/components/Content/useCatalog.tsx
+++ web/app/components/Content/useCatalog.tsx
@@ -3,16 +3,16 @@ import { api } from "../utils/api";
 import { Book } from "../Types/cart";
 
 export default function useCatalog() {
-        const [catalog, setCatalog] = useState<Record<string, Book>>({});
+    const [catalog, setCatalog] = useState<Record<string, Book>>({});
 
-        const requestCatalog = useCallback(async () => {
-                try {
-                        const items = await api.catalog();
-                        setCatalog(items);
-                } catch (error) {
-                        console.error("Catalog initialization failed:", error);
-                }
-        }, []);
+    const requestCatalog = useCallback(async () => {
+        try {
+            const items = await api.catalog();
+            setCatalog(items);
+        } catch (error) {
+            throw error;
+        }
+    }, []);
 
-        return { catalog, requestCatalog };
+    return { catalog, requestCatalog };
 }
