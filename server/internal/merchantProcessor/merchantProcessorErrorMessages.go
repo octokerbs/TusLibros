e515d796248c68088d6cf38cf4b5eb747ac2795d
Octavio Kerbs <kerbsod@gmail.com>
2025-01-19 20:36:30 -0300

    Removed joke

--- server/internal/merchantProcessor/merchantProcessorErrorMessages.go
+++ server/internal/merchantProcessor/merchantProcessorErrorMessages.go
@@ -2,4 +2,4 @@ package merchantProcessor
 
 const InvalidCreditCardErrorMessage = "Credit card number is invalid"
 const ExpiredCreditCardErrorMessage = "Credit card is expired"
-const NoFundsCreditCardErrorMessage = "Los de 10 pines no te depositaron otra vez?"
+const NoFundsCreditCardErrorMessage = "Credit card has no funds"
